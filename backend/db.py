c5e49a480a15728c870bf401d3e67b8afc633119
Anusri <anusridhanabal007@gmail.com>
2026-06-20 12:55:11 +0530

    Fixed dashboard navigation links

--- backend/db.py
+++ backend/db.py
@@ -1,18 +1,21 @@
 import mysql.connector
+import os
 
 db = None
 cursor = None
 
 try:
     db = mysql.connector.connect(
-        host="localhost",
-        user="vmsuser",
-        password="vms123",
-        database="VMS"
+         host="mysql-1ce8f6d7-anusridhanabal007-112d.a.aivencloud.com",
+        user="avnadmin",
+        password = os.getenv("DB_PASSWORD"),
+        database="defaultdb",
+        port=10936,
+        ssl_disabled=False
     )
 
     cursor = db.cursor()
     print("Database Connected Successfully")
 
 except Exception as e:
-    print("Database Connection Skipped:", e)
+    print("Database Connection Failed:", e)
